279207adb39d84f16e998332d14d97bfc2f9a0ba
JohnLYYN123 <96747053+JohnLYYN123@users.noreply.github.com>
2023-10-18 17:40:42 -0400

    Delete assignment2/folder_init directory

--- assignment2/folder_init/run_tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-! python3.9 -m pytest test_p1_public.py
-! python3.9 -m pytest test_p2_public.py
-! python3.9 -m pytest test_p3_public.py
-! python3.9 -m pytest test_p4_public_edge.py
-! python3.9 -m pytest test_p4_public_dist.py
-! python3.9 -m pytest test_p4_public_steps.py
